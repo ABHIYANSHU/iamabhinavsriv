116ab7cf610ffff3922cea738f54359bc2f69bcb
Abhinav Srivastava <abhiyanshu11.Srivastava@Honeywell.com>
2020-11-14 00:24:41 +0530

    Initial Resume

---
+++ src/resumeData.js
@@ -0,0 +1,109 @@
+export const DATA = 
+{
+  "main": {
+    "name":"Abhinav Srivastava",
+    "description":"I am a Full Stack Developer",
+    "image":"ProfilePic.png",
+    "bio":"I am a Software Developer with skills in Java, ReactJS, C#, Python, RPA and Project Management.",
+    "contactmessage":"Contact Me",
+    "email": "iamabhinavsriv@gmail.com",
+    "phone": "+91 8090-594-331",
+    "github":"https://github.com/ABHIYANSHU/",
+    "linkedin":"https://www.linkedin.com/in/abhiyanshu/",
+    "address":{
+      "Locality":"HSR",
+      "city":"Bangalore",
+      "state":"Karnataka",
+      "zip": "560102"
+    },
+    "website": "https://abhiyanshu.github.io/iamabhinavsriv/",
+    "resumedownload":"https://drive.google.com/file/d/1UtWVKSMea9uNODvHnlf4ZbwEu1-3DeJt/view?usp=sharing",
+    "social":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/abhiyanshu/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/ABHIYANSHU/",
+        "className":"fa fa-github"
+      }
+    ]
+  },
+  "resume":{
+    "skillmessage":"My Resume",
+    "education":[
+      {
+        "school":"Shri Ram Murti Smarak College of Engg and Technology, Bareilly",
+        "degree":"B-Tech in Computer Science and Engg",
+        "graduated":"June 2017",
+        "description":""
+      },
+      {
+        "school":"W.H. Smith Memorial School, Varanasi",
+        "degree":"High School & Intermediate",
+        "graduated":"March 2011 & March 2013",
+        "description":""
+      }
+    ],
+    "work":[
+      {
+        "company":"Honeywell Solutions Lab",
+        "title":"Senior Software Engineer",
+        "years":"April 2019 - Present",
+        "description":"Bangalore"
+      },
+      {
+        "company":"Tata Consultancy Services",
+        "title":"Assistant System Engineer",
+        "years":"August 2017 - April 2019",
+        "description":"Bangalore"
+      }
+    ],
+    "skills":[
+      {
+        "name":"Java",
+        "level":"95%"
+      },
+      {
+        "name":"Spring Boot",
+        "level":"80%"
+      },
+      {
+        "name":"ReactJs",
+        "level":"66%"
+      },
+      {
+        "name":"C#",
+        "level":"90%"
+      },
+      {
+        "name":".NET",
+        "level":"70%"
+      },
+      {
+        "name":"HTML, CSS",
+        "level":"90%"
+      },
+      {
+        "name":"Bootstrap",
+        "level":"85%"
+      },
+      {
+        "name":"Javascript",
+        "level":"70%"
+      }
+    ]
+  },
+  "portfolio":{
+    "projects": [
+      {
+        "title":"Data Structure & Algorithms",
+        "category":"A guide to DS and ALgo",
+        "image":"",
+        "url":""
+      }
+    ]
+  }
+}
